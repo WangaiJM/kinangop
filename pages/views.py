cbe026190f5aaef8951f4ef5a59facc6035eec0a
WangaiJM <jahkeyjohn@gmail.com>
2022-12-16 00:47:28 +0300

    vision

--- pages/views.py
+++ pages/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'home.html')
+def about(request):
+    return render(request, 'about.html')
+def vision(request):
+    return render(request, 'vision.html')
