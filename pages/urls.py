edf27f273427d3fdf67365c0a13410b8aa2d3a56
WangaiJM <jahkeyjohn@gmail.com>
2022-12-17 00:30:08 +0300

    template layout

--- pages/urls.py
+++ pages/urls.py
@@ -4,6 +4,17 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    # about
     path('about/', views.about, name='about'),
     path('vision/', views.vision, name='vision'),
+    # admissions
+    path('gen-procedure/', views.procedure, name='procedures'),
+    path('programs/', views.programs, name='programs'),
+    path('adminssion-downloads/', views.admissionDownloads, name='admission-downloads'),
+    path('online-registration/', views.onlineReg, name='online-registration'),
+    # students
+    path('student-downloads/', views.studentDownloads, name='student-downloads'),
+    path('welfare/', views.welfare, name="welfare"),
+    # contacts
+    path('contacts/', views.contacts, name="contacts"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,7 +2,27 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'home.html')
+
+# About
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'about/about.html')
 def vision(request):
-    return render(request, 'vision.html')
+    return render(request, 'about/vision.html')
+
+# Admissions
+def procedure(request):
+    return render(request, 'admissions/procedure.html')
+def programs(request):
+    return render(request, 'admissions/programs.html')
+def admissionDownloads(request):
+    return render(request, 'admissions/admission-downloads.html')
+def onlineReg(request):
+    return render(request,'admissions/online-reg.html')
+# students
+def studentDownloads(request):
+    return render(request, 'students/student-downloads.html')
+def welfare(request):
+    return render(request, 'students/welfare.html')
+# contacts
+def contacts(request):
+    return render(request, 'contacts/contact.html')
