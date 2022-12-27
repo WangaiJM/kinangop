cc87f60357a0d702d9553d7d1c77238f1fb75a83
WangaiJM <jahkeyjohn@gmail.com>
2022-12-27 17:57:33 +0300

    opportunity download

--- opportunity/views.py
+++ opportunity/views.py
@@ -2,16 +2,16 @@ from django.shortcuts import render
 from .models import tender, career, internship
 
 def tenderView(request):
-    tenders = tender.objects.all()
+    tenders = tender.objects.all().order_by('-uploaded_at')
     context = {'tenders':tenders}
     return render(request, 'tenders.html', context)
 
 def careerView(request):
-    careers = career.objects.all()
+    careers = career.objects.all().order_by('-uploaded_at')
     context = {'careers': careers}
     return render(request, 'careers.html', context)
 
 def internshipView(request):
-    internships = internship.objects.all()
+    internships = internship.objects.all().order_by('-uploaded_at')
     context = {'internships':internships}
     return render(request, 'internships.html', context)
