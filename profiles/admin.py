0f502f6e7808a808071edd5b7b145886bca6f294
WangaiJM <jahkeyjohn@gmail.com>
2023-01-17 20:42:06 +0300

    guardian info

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import student_profile, trainer_profile
+from .models import student_profile, trainer_profile, guardianInfo
 
 @admin.register(student_profile)
 class studentProfileAdmin(admin.ModelAdmin):
@@ -15,6 +15,15 @@ class studentProfileAdmin(admin.ModelAdmin):
     list_display = ['trainer_id', 'full_name', 'email', 'id_number', 'contact']
     search_fields = ['full_name', 'department__department_name']
     list_filter = ['department__department_name', 'gender']
+
+    def full_name(self, obj):
+        return '{} {} {}'.format(obj.firstname, obj.middlename, obj.lastname)
+
+@admin.register(guardianInfo)
+class studentProfileAdmin(admin.ModelAdmin):
+    list_display = ['student_id', 'full_name', 'email', 'id_number', 'contact']
+    search_fields = ['full_name', 'contact']
+    list_filter = [ 'gender']
 
     def full_name(self, obj):
         return '{} {} {}'.format(obj.firstname, obj.middlename, obj.lastname)
--- profiles/models.py
+++ profiles/models.py
@@ -12,8 +12,8 @@ class student_profile(models.Model):
     email = models.EmailField(max_length=150)
     contact = models.CharField(max_length=150)
     gender = models.CharField(max_length=150, choices=[('Male', 'Male'), ('Female', 'Female')])
-    Box_number = models.CharField(max_length=150)
-    Postal_code = models.CharField(max_length=150)
+    box_number = models.CharField(max_length=150)
+    postal_code = models.CharField(max_length=150)
     town = models.CharField(max_length=150)
     course = models.OneToOneField('department.course', on_delete=models.CASCADE)
     passport = models.ImageField(upload_to='students/passport/', blank=True,null=True)
@@ -29,7 +29,7 @@ class student_profile(models.Model):
         verbose_name_plural = 'Student Profiles'
 
 class trainer_profile(models.Model):
-    trainer_id = models.OneToOneField(User, on_delete=models.CASCADE)
+    trainer_id = models.ForeignKey(student_profile, on_delete=models.CASCADE)
     firstname = models.CharField(max_length=150)
     middlename = models.CharField(max_length=150, null=True, blank=True)
     lastname = models.CharField(max_length=150)
@@ -37,8 +37,8 @@ class trainer_profile(models.Model):
     email = models.EmailField(max_length=150)
     contact = models.CharField(max_length=150)
     gender = models.CharField(max_length=150, choices=[('Male', 'Male'), ('Female', 'Female')])
-    Box_number = models.CharField(max_length=150)
-    Postal_code = models.CharField(max_length=150)
+    box_number = models.CharField(max_length=150)
+    postal_code = models.CharField(max_length=150)
     town = models.CharField(max_length=150)
     department = models.OneToOneField('department.department', on_delete=models.CASCADE)
     passport = models.ImageField(upload_to='teacher/passport/', blank=True, null=True)
@@ -51,3 +51,24 @@ class trainer_profile(models.Model):
     class Meta:
         verbose_name = 'Trainer Profile'
         verbose_name_plural = 'Trainer Profiles'
+
+class guardianInfo(models.Model):
+    student = models.ForeignKey(User, on_delete=models.CASCADE)
+    firstname = models.CharField(max_length=150)
+    middlename = models.CharField(max_length=150, null=True, blank=True)
+    lastname = models.CharField(max_length=150)
+    id_number = models.CharField(max_length=150)
+    email = models.EmailField(max_length=150, blank=True, null=True)
+    contact = models.CharField(max_length=150)
+    gender = models.CharField(max_length=150, choices=[('Male', 'Male'), ('Female', 'Female')])
+    box_number = models.CharField(max_length=150)
+    postal_code = models.CharField(max_length=150)
+    town = models.CharField(max_length=150)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.lastname
+    
+    class Meta:
+        verbose_name = 'Guardian'
+        verbose_name_plural = 'Guardians'
